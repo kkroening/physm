d04eecbad2c8d15bcd3be65f69f732c92abafff0
Karl Kroening <karlk@kralnet.us>
2020-11-02 02:34:50 -0600

    physm-rs: use usize as primary indexing strategy instead of IDs

--- physm-rs/src/solver.rs
+++ physm-rs/src/solver.rs
@@ -13,9 +13,11 @@ pub struct Solver {
     pub runge_kutta: bool,
 }
 
-type FrameRefVec<'a> = Vec<&'a Box<dyn Frame>>;
-type FramePath<'a> = Vec<&'a FrameId>;
-type FrameIdPathMap<'a> = HashMap<&'a FrameId, FramePath<'a>>;
+type FrameBox = Box<dyn Frame>;
+type FrameRefVec<'a> = Vec<&'a FrameBox>;
+type FramePath = Vec<usize>;
+type FrameIdIndexMap<'a> = HashMap<&'a FrameId, usize>;
+type FrameIndexPathMap = HashMap<usize, Vec<usize>>;
 
 impl Solver {
     fn sort_frames(frames: &Vec<Box<dyn Frame>>) -> FrameRefVec {
@@ -35,40 +37,70 @@ impl Solver {
         sorted_frames
     }
 
-    fn get_frame_id_path_map<'a>(frames: &'a FrameRefVec) -> FrameIdPathMap<'a> {
-        fn visit<'a>(
-            frame: &'a Box<dyn Frame>,
-            mut path: FramePath<'a>,
-            frame_id_path_map: &mut FrameIdPathMap<'a>,
+    fn get_id_index_map<'a>(frames: &'a FrameRefVec) -> FrameIdIndexMap<'a> {
+        let mut id_index_map = HashMap::new();
+        id_index_map.reserve(frames.len());
+        frames.iter().enumerate().for_each(|(index, frame)| {
+            id_index_map.insert(frame.get_id(), index);
+        });
+        id_index_map
+    }
+
+    fn get_index_path_map(frames: &FrameRefVec) -> FrameIndexPathMap {
+        fn visit(
+            frame: &Box<dyn Frame>,
+            mut path: FramePath,
+            id_index_map: &FrameIdIndexMap,
+            index_path_map: &mut FrameIndexPathMap,
         ) {
-            let id = frame.get_id();
-            if !frame_id_path_map.contains_key(id) {
-                path.push(id);
-                println!("{:?}", path);
-                frame_id_path_map.insert(id, path);
+            let index = id_index_map[frame.get_id()];
+            if !index_path_map.contains_key(&index) {
+                path.push(index);
+                index_path_map.insert(index, path);
                 frame.get_children().iter().for_each(|child| {
                     visit(
                         child,
-                        frame_id_path_map.get(id).unwrap().to_owned(),
-                        frame_id_path_map,
+                        index_path_map.get(&index).unwrap().to_owned(),
+                        id_index_map,
+                        index_path_map,
                     )
                 });
             }
         }
 
-        let mut frame_id_path_map = HashMap::new();
-        frame_id_path_map.reserve(frames.len());
+        let id_index_map = Self::get_id_index_map(frames);
+        let mut index_path_map = HashMap::new();
+        index_path_map.reserve(frames.len());
         frames
             .iter()
-            .for_each(|frame| visit(&frame, Vec::new(), &mut frame_id_path_map));
-        frame_id_path_map
+            .for_each(|frame| visit(&frame, Vec::new(), &id_index_map, &mut index_path_map));
+        index_path_map
     }
 
-    fn get_pos_mats(states: &[f64], frames: &FrameRefVec) -> Vec<Matrix> {
+    fn get_parent_index(child_index: usize, index_path_map: &FrameIndexPathMap) -> Option<usize> {
+        let path = &index_path_map[&child_index];
+        if path.len() > 1 {
+            Some(path[path.len() - 2])
+        } else {
+            None
+        }
+    }
+
+    //fn get_parent(child_index: usize,
+
+    fn get_pos_mats(
+        states: &[f64],
+        frames: &FrameRefVec,
+        index_path_map: &FrameIndexPathMap,
+    ) -> Vec<Matrix> {
+        let get_parent_index = |index| Self::get_parent_index(index, index_path_map);
         frames
             .iter()
             .enumerate()
-            .map(|(index, frame)| frame.get_local_pos_matrix(states[index]))
+            .map(|(index, frame)| {
+                let parent_index = get_parent_index(index);
+                frame.get_local_pos_matrix(states[index])
+            })
             .collect()
     }
 
@@ -85,7 +117,8 @@ impl Solver {
     }
 
     pub fn tick(&self, states: &[f64], delta_time: f64) -> i32 {
-        let sorted_frames = Self::sort_frames(&self.scene.frames);
+        let frames = Self::sort_frames(&self.scene.frames);
+        let index_path_map = Self::get_index_path_map(&frames);
         42
     }
 }
@@ -97,11 +130,11 @@ mod tests {
 
     fn get_sample_frames() -> Vec<Box<dyn Frame>> {
         vec![
-            Box::new(TrackFrame::new("4".into()).add_child(Box::new(TrackFrame::new("5".into())))),
+            Box::new(TrackFrame::new("3".into()).add_child(Box::new(TrackFrame::new("4".into())))),
             Box::new(
-                TrackFrame::new("1".into())
-                    .add_child(Box::new(TrackFrame::new("3".into())))
-                    .add_child(Box::new(TrackFrame::new("2".into()))),
+                TrackFrame::new("0".into())
+                    .add_child(Box::new(TrackFrame::new("2".into())))
+                    .add_child(Box::new(TrackFrame::new("1".into()))),
             ),
         ]
     }
@@ -113,20 +146,20 @@ mod tests {
                 .iter()
                 .map(|frame| frame.get_id().as_str())
                 .collect::<Vec<&str>>(),
-            vec!["1", "2", "3", "4", "5"]
+            vec!["0", "1", "2", "3", "4"]
         );
     }
 
     #[test]
-    fn test_get_frame_id_path_map() {
+    fn test_get_index_path_map() {
         let frames = get_sample_frames();
         let frames = Solver::sort_frames(&frames);
-        let frame_id_path_map = Solver::get_frame_id_path_map(&frames);
-        let mut items = frame_id_path_map.iter().collect::<Vec<_>>();
-        items.sort_by_key(|(k, v)| k.to_owned());
+        let index_path_map = Solver::get_index_path_map(&frames);
+        let mut items = index_path_map.iter().collect::<Vec<_>>();
+        items.sort_by_key(|(k, v)| *k);
         assert_eq!(
             format!("{:?}", items),
-            r#"[("1", ["1"]), ("2", ["1", "2"]), ("3", ["1", "3"]), ("4", ["4"]), ("5", ["4", "5"])]"#,
+            "[(0, [0]), (1, [0, 1]), (2, [0, 2]), (3, [3]), (4, [3, 4])]",
         );
     }
 
